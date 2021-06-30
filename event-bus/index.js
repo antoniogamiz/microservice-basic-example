19de4af83222a126c4391eb344bc654007743bf7
antoniogamiz <antoniogamiz10@gmail.com>
2021-06-30 20:25:01 +0200

    feat: implement query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,9 +13,9 @@ app.post("/events", (req, res) => {
   axios
     .post("http://localhost:4001/events", event)
     .catch((err) => console.log(err));
-  // axios
-  //   .post("http://localhost:4002/events", event)
-  //   .catch((err) => console.log(err));
+  axios
+    .post("http://localhost:4002/events", event)
+    .catch((err) => console.log(err));
 
   res.send({ status: "OK" });
 });
---
+++ query/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("Event received", type);
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listenning on 4002");
+});
